4858ee1b08341d3a0d404192deadc70ce7b73c4e
Manuel Santiago <manuel_sm223@hotmail.com>
2021-02-07 23:21:04 -0600

    styles added and changes in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,22 @@
 import { createTask } from './items.js';
 
+const divApp = document.createElement('div');
+const divAddTask = document.createElement('div');
 const input = document.createElement('input');
 const button = document.createElement('button');
 
 input.placeholder = 'New task...';
+input.className = 'form-control';
 button.textContent = 'Add';
+button.className = 'btn btn-outline-primary';
 
 const h1 = document.createElement('h1');
 h1.textContent = 'Todo App';
+
+divAddTask.append(input, button);
+divAddTask.className = 'input-group';
+divApp.append(h1, divAddTask);
+divApp.className = 'mb-2';
 
 const div = document.createElement('div');
 
@@ -19,6 +28,7 @@ button.addEventListener('click', function(event) {
 });
 
 const section = document.createElement('section');
-section.append(h1, input, button, div);
+section.className = 'container text-center my-4 col-md-4';
+section.append(divApp, div);
 
 export default section;
